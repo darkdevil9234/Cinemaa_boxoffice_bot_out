386703ced8753484f9e75c849540b8a6b69661cb
8576-09917 <132528322+8576-09917@users.noreply.github.com>
2023-05-04 17:21:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bothub517:password1234@cluster0.kpst8de.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Alok:password1234@cluster0.etlxggy.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cinemaa_boxoffice")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
