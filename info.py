bf76a0a0485a63790ce198fb8b3760ef418b6771
8576-09917 <132528322+8576-09917@users.noreply.github.com>
2023-05-08 09:23:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,6 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-VERIFY2 URL = environ.get('VERIFY2_URL', "shorturllink.in") 
-VERIFY2 API = environ.get('VERIFY2_API', "903c2db62e3e05d66edefa1e0b763469ece42329")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shorturllink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '903c2db62e3e05d66edefa1e0b763469ece42329')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
