3e994cc81492a2e24877fd521ec40b552f3806d1
8576-09917 <132528322+8576-09917@users.noreply.github.com>
2023-05-08 10:39:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,9 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "cinemaa_boxoffice")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
+VERIFY2_URL = environ.get('VERIFY2_URL', "shorturllink.in")
+VERIFY2_API = environ.get('VERIFY2_API', "903c2db62e3e05d66edefa1e0b763469ece42329")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shorturllink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '903c2db62e3e05d66edefa1e0b763469ece42329')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
